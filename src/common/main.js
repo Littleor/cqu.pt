c2d4411c540bf02d2eedaa980f2a620321740020
Cong Min <i@congm.in>
2016-09-17 23:49:12 +0800

    update

--- src/common/main.js
+++ src/common/main.js
@@ -33,83 +33,12 @@ var _hmt = _hmt || [];
     }
     /* 插入dom */
     css.onload = function(){
-        var a = require('../templates.html');
+        var templates = require('../templates/templates.html');
         if(self == top){
-            document.body.insertAdjacentHTML("afterbegin", a);
+            document.body.insertAdjacentHTML("afterbegin", templates);
             document.body.classList.add('_cqupt-body');
+            require('../templates/templates');
         }
-        /* side-box */
-        var title_btn = document.querySelector("#_cqupt-title"),
-            discuss_btn = document.querySelector("#_cqupt-discuss"),
-            donate_btn = document.querySelector("#_cqupt-donate-btn"),
-            info_btn = document.querySelector("#_cqupt-info-btn"),
-            duosuo = document.querySelector("#_cqupt-duosuo"),
-            donate = document.querySelector("#_cqupt-donate"),
-            info = document.querySelector("#_cqupt-info"),
-            side = document.querySelector("#_cqupt-side-box"),
-            close = document.querySelector("#_cqupt-discuss-close");
-        title_btn.onclick = function(){
-            document.body.classList.remove('_cqupt-body');
-            side.classList.remove('_cqupt-active');
-            side.classList.add('_cqupt-close');
-        };
-        discuss_btn.onclick = function(){
-            var el = document.createElement('div');
-            el.setAttribute('data-thread-key', '1');
-            el.setAttribute('data-title', '内网外入');
-            el.setAttribute('data-url', 'https://cqupt.congm.in');
-            DUOSHUO.EmbedThread(el);
-            var duosuo_content = duosuo.querySelector("._cqupt-content-bd");
-            duosuo_content.replaceChild(el, duosuo_content.lastElementChild);
-            discuss_btn.classList.add('_cqupt-active');
-            donate_btn.classList.remove('_cqupt-active');
-            info_btn.classList.remove('_cqupt-active');
-            side.classList.add('_cqupt-active');
-            duosuo.classList.remove('_cqupt-hidden');
-            donate.classList.add('_cqupt-hidden');
-            info.classList.add('_cqupt-hidden');
-        };
-        donate_btn.onclick = function(){
-            discuss_btn.classList.remove('_cqupt-active');
-            donate_btn.classList.add('_cqupt-active');
-            info_btn.classList.remove('_cqupt-active');
-            side.classList.add('_cqupt-active');
-            duosuo.classList.add('_cqupt-hidden');
-            donate.classList.remove('_cqupt-hidden');
-            info.classList.add('_cqupt-hidden');
-        };
-        info_btn.onclick = function(){
-            discuss_btn.classList.remove('_cqupt-active');
-            donate_btn.classList.remove('_cqupt-active');
-            info_btn.classList.add('_cqupt-active');
-            side.classList.add('_cqupt-active');
-            duosuo.classList.add('_cqupt-hidden');
-            donate.classList.add('_cqupt-hidden');
-            info.classList.remove('_cqupt-hidden');
-        };
-        close.onclick = function(){
-            discuss_btn.classList.remove('_cqupt-active');
-            donate_btn.classList.remove('_cqupt-active');
-            info_btn.classList.remove('_cqupt-active');
-            side.classList.remove('_cqupt-active');
-            duosuo.classList.add('_cqupt-hidden');
-            donate.classList.add('_cqupt-hidden');
-            info.classList.add('_cqupt-hidden');
-        };
-        // donate.json
-        (function(data){
-            var html = '<tbody>';
-            for(var i = 0; i < data.length; i++){
-                html += '<tr>';
-                html += '<td>' + data[i].user_id +'</td>';
-                html += '<td>' + data[i].user_name +'</td>';
-                html += '<td>' + data[i].time +'</td>';
-                html += '<td>' + data[i].money +'</td>';
-                html += '</tr>';
-            }
-            html += '</tbody>';
-            document.querySelector("._cqupt-donate-list").insertAdjacentHTML("afterbegin", html);
-        })(require('../../json/donate'));
     };
 })();
 
---
+++ src/templates/templates.js
@@ -0,0 +1,57 @@
+(function(){
+    /* side-box */
+    var title_btn = document.querySelector("#_cqupt-title"),
+        discuss_btn = document.querySelector("[data-target='#_cqupt-duosuo']"),
+        duosuo = document.querySelector("#_cqupt-duosuo"),
+        side = document.querySelector("#_cqupt-side-box");
+    var sideTabs = document.querySelectorAll("[data-toggle='sideTab']"),
+        sideTabLen = sideTabs.length,
+        contentList = document.querySelectorAll("._cqupt-content-item"),
+        contentLen = contentList.length;
+    side.addEventListener('click', function(e){
+        if(!e.target){ return; }
+        var eTarget = e.target.getAttribute('data-toggle') == 'sideTab' ? e.target : e.target.parentNode;
+        if(eTarget.getAttribute('data-toggle') == 'sideTab'){
+            for(var i = 0; i < sideTabLen; i++){
+                sideTabs[i].classList.remove('_cqupt-active');
+            }
+            for(var j = 0; j < contentLen; j++){
+                contentList[j].classList.add('_cqupt-hidden');
+            }
+            var tabTarget = eTarget.getAttribute('data-target');
+            if(tabTarget != 'close'){
+                eTarget.classList.add('_cqupt-active');
+                side.classList.add('_cqupt-active');
+                document.querySelector(tabTarget).classList.remove('_cqupt-hidden');
+            }
+        }
+    });
+    title_btn.onclick = function(){
+        document.body.classList.remove('_cqupt-body');
+        side.classList.remove('_cqupt-active');
+        side.classList.add('_cqupt-close');
+    };
+    discuss_btn.addEventListener('click', function(){
+        var el = document.createElement('div');
+        el.setAttribute('data-thread-key', '1');
+        el.setAttribute('data-title', '内网外入');
+        el.setAttribute('data-url', 'https://cqupt.congm.in');
+        DUOSHUO.EmbedThread(el);
+        var duosuo_content = duosuo.querySelector("._cqupt-content-bd");
+        duosuo_content.replaceChild(el, duosuo_content.lastElementChild);
+    });
+    // donate.json
+    (function(data){
+        var html = '<tbody>';
+        for(var i = 0; i < data.length; i++){
+            html += '<tr>';
+            html += '<td>' + data[i].user_id +'</td>';
+            html += '<td>' + data[i].user_name +'</td>';
+            html += '<td>' + data[i].time +'</td>';
+            html += '<td>' + data[i].money +'</td>';
+            html += '</tr>';
+        }
+        html += '</tbody>';
+        document.querySelector("._cqupt-donate-list").insertAdjacentHTML("afterbegin", html);
+    })(require('../../json/donate'));
+})();
