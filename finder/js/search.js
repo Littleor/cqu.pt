5445e3683f7238d3dc7cf699aea38d63ed536eeb
Cong Min <i@congm.in>
2016-09-19 17:29:23 +0800

    update

--- finder/js/search.js
+++ finder/js/search.js
@@ -16,6 +16,112 @@ $(".logo").click(function(){
     $(".header").removeClass("top");
     $(".list").hide("fast");
 });
+// 用户信息
+var CrossStorageClient = require('cross-storage').CrossStorageClient;
+var cqupt_inner_storage = new CrossStorageClient('https://cqupt.congm.in/common/storage-iframe.html');
+cqupt_inner_storage.onConnect().then(function(){
+    window._cqupt_inner_finder = cqupt_inner_storage.get('cqupt_inner_finder');
+    if(!window._cqupt_inner_finder){
+        _cqupt_inner_finder = 0;
+    }
+    return cqupt_inner_storage.get('cqupt_inner');
+}).then(function(res) {
+    var cqupt_inner = {};
+    if(!res){
+        cqupt_inner_storage.set('cqupt_inner', JSON.stringify({}));
+    }
+    try{
+        cqupt_inner = JSON.parse(res);
+    }catch(error){
+        cqupt_inner_storage.set('cqupt_inner', JSON.stringify({}));
+        cqupt_inner = {};
+    }
+    // 获取用户信息
+    var user_xh = 0,
+        user_xh_count = 3;
+    if(cqupt_inner.xh_list){
+        var xh_list = cqupt_inner.xh_list;
+        for(var i in xh_list){
+            if(xh_list.hasOwnProperty(i)){
+                if(xh_list[i] >= user_xh_count){
+                    user_xh = i;
+                    user_xh_count = xh_list[i];
+                }
+            }
+        }
+    }
+    // 查询用户信息
+    if(parseInt(user_xh)){
+        var request = new XMLHttpRequest();
+        request.open('GET', 'https://blues.congm.in/stu.php?searchKey=' + parseInt(user_xh), true);
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                var data = JSON.parse(request.responseText);
+                if(data.total === 1){
+                    window._cqupt_inner_user = data.rows[0];
+                    _cqupt_inner_user_show();
+                }
+            }else{
+                window._cqupt_inner_user = {};
+            }
+        };
+        request.onerror = function() {
+            window._cqupt_inner_user = {};
+        };
+        request.send();
+    }
+});
+function _cqupt_inner_user_show(){
+    //顶部悬浮提醒框
+    var warn = $("#warn"), warn_text = $("#warn-text");
+    var s = warn.get(0), si = warn_text.get(0);
+    var s_scrollLeft, s_add = 1, tmar;
+    //文字滑动
+    function mar() {
+        if (s.offsetWidth <= si.offsetWidth) {
+            s_scrollLeft = s.scrollLeft;
+            s.scrollLeft += s_add;
+            if (s_scrollLeft == s.scrollLeft) {
+                s_add = -s_add;
+            }
+            tmar = setTimeout(mar, 20);
+        }
+    }
+    //框体升降
+    var warnBox = {
+        up: function () {
+            warn.animate({"top": "-48px"}, "slow");
+            clearTimeout(tmar);
+        },
+        down: function (text) {
+            var t = {
+                showTime: 500,
+                moveTime: 4000,
+                totalTime: 15000
+            };
+            $.each(text, function (i, e) {
+                setTimeout(function () {
+                    s.scrollLeft = 0;
+                    warn_text.html(e);
+                    warn.animate({"top": "18px"}, "slow");
+                }, t.totalTime * i + t.showTime);
+                setTimeout(function () {
+                    mar();
+                }, t.totalTime * i + t.moveTime);
+                setTimeout(function () {
+                    warnBox.up();
+                }, t.totalTime * i + t.totalTime);
+            });
+        }
+    };
+    var warn_text_d = '同学，您好。你已累计进行过' + _cqupt_inner_finder + '次搜索，请谨慎使用Finder！';
+    if(_cqupt_inner_user.xm){
+        warn_text_d = _cqupt_inner_user.xm + warn_text_d
+    }
+    var warn_text_array = [warn_text_d];
+    warnBox.down(warn_text_array);
+}
+
 function search(key){
     if(!key.trim()){
         $(".logo").trigger('click');
@@ -60,4 +166,7 @@ function search(key){
             $(".info-box").slideDown("fast");
         }
     });
+    // 收集用户点击次数
+    _cqupt_inner_finder++;
+    cqupt_inner_storage.set('cqupt_inner_finder', _cqupt_inner_finder);
 }
