3bdeb6658128e21d4d3f5b83ecbbc673c7a842b7
Cong Min <i@congm.in>
2016-10-12 14:25:18 +0800

    update

--- src/templates/storage-iframe.js
+++ src/templates/storage-iframe.js
@@ -1,4 +1,5 @@
 var CrossStorageHub = require('cross-storage').CrossStorageHub;
 CrossStorageHub.init([
-    { origin: /^(.*)host\.congm\.in$/, allow: ['get', 'set', 'del', 'getKeys', 'clear'] }
+    { origin: /^(.*)host\.congm\.in\:88$/, allow: ['get', 'set', 'del', 'getKeys', 'clear'] },
+    { origin: /^(.*)cqupt\.congm\.in$/, allow: ['get', 'set', 'del', 'getKeys', 'clear'] }
 ]);
