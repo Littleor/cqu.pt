c18dd0488a1a77bbc840630c22837ad2c3a6acc7
Cong Min <i@congm.in>
2017-04-14 10:18:57 +0800

    update

--- status.php
+++ status.php
@@ -18,8 +18,9 @@ curl_exec( $ch );
 
 $ip = gethostbyname( $host );
 $ping = curl_getinfo( $ch, CURLINFO_CONNECT_TIME );
+$code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
 
-echo json_encode( [ $ip, ceil($ping * 1000) ] );
+echo json_encode( [ $ip, ceil($ping * 1000), $code ] );
 
 curl_close( $ch );
 
