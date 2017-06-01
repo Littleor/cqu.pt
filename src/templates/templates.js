4435532c078bbd144a320fa84a0e4bb339d47a98
Cong Min <i@congm.in>
2017-06-01 14:11:20 +0800

    update

--- src/templates/templates.js
+++ src/templates/templates.js
@@ -1,46 +1,22 @@
-/*广告
-(function() {
-    var google = document.createElement("script");
-    google.async = "true";
-    google.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-    if(self == top){
-        document.body.appendChild(google);
-    }
-    var ibody = document.querySelector('body'), adbox = document.querySelector('#_cqupt-adbox');
-    if(getComputedStyle(adbox, null).display != 'none'){
-        // 3个内容广告
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    }
-    // 移动端网页级广告
-    (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-9908918414837596",
-        enable_page_level_ads: true
-    });
-    function outerHeight(el) {
-        var height = el.offsetHeight;
-        var style = getComputedStyle(el);
-        height += parseInt(style.marginTop) + parseInt(style.marginBottom);
-        return height;
-    }
-    var wHeight = window.innerHeight, adHeight = outerHeight(document.querySelector('#_cqupt-adbox')), maxHeight = 0;
-    var bcs = ibody.children;
-    for(var i = 0; i < bcs.length; i++){
-        if(bcs[i].id.indexOf('_cqupt') === -1 && maxHeight < outerHeight(bcs[i])){
-            maxHeight = outerHeight(bcs[i]);
-        }
-    }
-    if(maxHeight){
-        ibody.style.minHeight = maxHeight + 'px';
-    }
-    if(wHeight > adHeight + maxHeight) {
-        adbox.style.top = (wHeight-adHeight) + 'px';
-    }
-    document.querySelector('#_cqupt-adbox-close').onclick = function(){
-        adbox.style.display = 'none';
-    };
-})();*/
+/*广告*/
+(function(win,doc){
+  var s = doc.createElement("script"), h = doc.getElementsByTagName("head")[0];
+  if (!win.alimamatk_show) {
+    s.charset = "gbk";
+    s.async = true;
+    s.src = "https://alimama.alicdn.com/tkapi.js";
+    h.insertBefore(s, h.firstChild);
+  }
+  var o = {
+    pid: "mm_96052862_26536335_104412020",/*推广单元ID，用于区分不同的推广渠道*/
+    appkey: "",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/
+    unid: "",/*自定义统计字段*/
+    type: "click" /* click 组件的入口标志 （使用click组件必设）*/
+  };
+  win.alimamatk_onload = win.alimamatk_onload || [];
+  win.alimamatk_onload.push(o);
+})(window,document);
+
 (function(){
     /* 更改标题 */
     if(location.hostname !== 'cqupt.congm.in') { document.title = '【内网外入】 ' + document.title; }
