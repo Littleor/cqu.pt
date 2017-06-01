4c72e57a8da1b5db201f775b89ed83e867eca699
Cong Min <i@congm.in>
2017-06-01 15:49:48 +0800

    update

--- src/templates/templates.js
+++ src/templates/templates.js
@@ -9,7 +9,7 @@
   }
   var o = {
     pid: "mm_96052862_26536335_104462258",/*推广单元ID，用于区分不同的推广渠道*/
-    appkey: "",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/
+    appkey: "23887875",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/
     unid: "",/*自定义统计字段*/
     type: "click" /* click 组件的入口标志 （使用click组件必设）*/
   };
