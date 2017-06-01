06d5cf6c60b6a561a39a01ea659f7581c6ceb4a1
Cong Min <i@congm.in>
2017-06-01 14:59:33 +0800

    update

--- src/templates/templates.js
+++ src/templates/templates.js
@@ -1,3 +1,22 @@
+/* 广告 */
+(function(win,doc){
+  var s = doc.createElement("script"), h = doc.getElementsByTagName("head")[0];
+  if (!win.alimamatk_show) {
+    s.charset = "gbk";
+    s.async = true;
+    s.src = "https://alimama.alicdn.com/tkapi.js";
+    h.insertBefore(s, h.firstChild);
+  }
+  var o = {
+    pid: "mm_96052862_26536335_104462258",/*推广单元ID，用于区分不同的推广渠道*/
+    appkey: "",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/
+    unid: "",/*自定义统计字段*/
+    type: "click" /* click 组件的入口标志 （使用click组件必设）*/
+  };
+  win.alimamatk_onload = win.alimamatk_onload || [];
+  win.alimamatk_onload.push(o);
+})(window,document);
+
 (function(){
     /* 更改标题 */
     if(location.hostname !== 'cqupt.congm.in') { document.title = '【内网外入】 ' + document.title; }
